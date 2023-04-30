895f0e17cd222e09fa39142f1eb17f06b9a2cc5c
Nicolas Boutet <amd3002@gmail.com>
2023-04-30 14:05:49 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,20 +5,27 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('telegraf'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("telegraf"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('username,groupname,path', [
-  ('telegraf', 'telegraf', '/etc/telegraf/telegraf.conf'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path",
+    [
+        ("telegraf", "telegraf", "/etc/telegraf/telegraf.conf"),
+    ],
+)
 def test_telegraf_config_file(host, username, groupname, path):
     config = host.file(path)
     assert config.exists
@@ -27,9 +34,12 @@ def test_telegraf_config_file(host, username, groupname, path):
     assert config.group == groupname
 
 
-@pytest.mark.parametrize('name', [
-  ('telegraf'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("telegraf"),
+    ],
+)
 def test_telegraf_service_is_running_and_enabled(host, name):
     service = host.service(name)
     assert service.is_enabled
